9d70242b2c2c7cd67053c37f425df81350fedbbc
TheFaser <artem.mochalin11@gmail.com>
2025-11-24 13:08:43 +0500

    test error

--- src/main/java/net/flectone/calculator/Calculator.java
+++ src/main/java/net/flectone/calculator/Calculator.java
@@ -16,7 +16,7 @@ public class Calculator {
 
     public double divide(int a, int b) {
         if (b == 0) {
-            throw new ArithmeticException("Division by zero is not allowed");
+            return 0;
         }
 
         return (double) a / b;
